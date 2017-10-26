1ddd59f144223958a6630d3455a6fb79311fea56
linyouchong <lin.youchong@zte.com.cn>
2017-10-27 01:32:19 +0800

    Remove unneccesory if clause

--- pkg/volume/metrics_statfs.go
+++ pkg/volume/metrics_statfs.go
@@ -46,11 +46,8 @@ func (md *metricsStatFS) GetMetrics() (*Metrics, error) {
 	}
 
 	err := md.getFsInfo(metrics)
-	if err != nil {
-		return metrics, err
-	}
 
-	return metrics, nil
+	return metrics, err
 }
 
 // getFsInfo writes metrics.Capacity, metrics.Used and metrics.Available from the filesystem info
